b9a91a13c8ea0503be1ada7440ddd56f02c39524
Efrexz <zyzz_448@hotmail.com>
2023-09-30 20:54:44 -0500

    useNavigate

--- src/BlogPost.jsx
+++ src/BlogPost.jsx
@@ -1,17 +1,23 @@
-import {  useParams } from "react-router-dom";
+import {  useNavigate, useParams } from "react-router-dom";
 import { blogsData } from "./blogsData.jsx";
 
 
 
 function BlogPost() {
     const params = useParams();
+    const navigate = useNavigate();
     console.log(params);
 
     const blogPost = blogsData.find(post => post.slug === params.slug);
 
+    function returnToBlogs(){
+        navigate("/blog")
+    }
+
     return(
         <>
             <h1>{blogPost.title}</h1>
+            <button onClick={returnToBlogs}>Volver a los blogs</button>
             <p>{blogPost.author}</p>
             <p>{blogPost.content}</p>
         </>
